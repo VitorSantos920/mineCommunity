9dc3948956138b0826f4c4a0ae63da8291f8fef6
Vitor Santos <pireshugo737@gmail.com>
2023-07-03 07:18:39 -0300

    :lipstick: Style: fieldset component
    add styles to fieldset component

--- src/components/Form/Fieldset/style.ts
+++ src/components/Form/Fieldset/style.ts
@@ -4,4 +4,12 @@ const { styled } = stitches;
 
 export const FieldsetContainer = styled('fieldset', {
   border: 'none',
+  marginBottom: 'calc($defaultSpace * 1.75)',
+
+  '&.form__checkbox': {
+    alignItems: 'center',
+    gap: 11,
+
+    '& label, & input': { cursor: 'pointer' },
+  },
 });
