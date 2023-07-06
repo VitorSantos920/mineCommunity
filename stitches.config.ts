100800ec0e4ec17c18f6cdaed0973c2fd301d97d
Vitor Santos <pireshugo737@gmail.com>
2023-07-06 04:16:47 -0300

    :iphone: Responsive: new breakpoint
    add new breakpoint: 400px

--- stitches.config.ts
+++ stitches.config.ts
@@ -30,6 +30,7 @@ export const stitches = createStitches({
   media: {
     bp625: '(max-width: 625px)',
     bp500: '(max-width: 500px)',
+    bp400: '(max-width: 400px)',
     bp320: '(max-width: 320px)',
   },
 });
