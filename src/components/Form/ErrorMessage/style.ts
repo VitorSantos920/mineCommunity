46dd6d26889bfa43fd05da69aa8e7f870315c4e5
Vitor Santos <pireshugo737@gmail.com>
2023-07-07 08:17:30 -0300

    :sparkles: Feat: ErrorMessage component
    add form error message component + styles + import to index file

---
+++ src/components/Form/ErrorMessage/style.ts
@@ -0,0 +1,8 @@
+import { stitches } from '../../../../stitches.config';
+
+const { styled } = stitches;
+
+export const ErrorMessage = styled('span', {
+  color: '$brandColor',
+  fontWeight: '$exb',
+});
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,11 +3,13 @@ import { FormFieldset } from './Fieldset';
 import { FormLabel } from './Label';
 import { FormInput } from './Input';
 import FormButton from './Button';
+import { FormErrorMessage } from './ErrorMessage';
 
 export const Form = {
   Root: FormRoot,
   Fieldset: FormFieldset,
   Label: FormLabel,
   Input: FormInput,
+  ErrorMessage: FormErrorMessage,
   Button: FormButton,
 };
