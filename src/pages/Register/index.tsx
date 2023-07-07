e69f8f699813217427d66dba13adf2f874b021cf
Vitor Santos <pireshugo737@gmail.com>
2023-07-07 10:04:55 -0300

    :sparkles: Feat: register user schema
    add register user schema for useForm hook resolver property setting

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -2,75 +2,82 @@ import { Link } from 'react-router-dom';
 import { Form } from '../../components/Form';
 import { InputControlContainer } from './style';
 import { Title } from '../../styles/Title';
+import { registerUserFormSchema } from '../../schemas/registerUserForm';
 
 export function Register() {
   return (
     <>
       <Title>Cadastre-se</Title>
-      <Form.Root>
+      <Form.Root schema={registerUserFormSchema}>
         <InputControlContainer className="form__name-container flex">
           <Form.Fieldset className="form__firstname">
             <Form.Label labelFor="firstname" text="Primeiro nome" />
             <Form.Input
               type="text"
-              required
               placeholder="Vitor"
-              name="firstname"
-              id="firstname"
+              name="firstName"
+              id="firstName"
             />
+            <Form.ErrorMessage field="firstName" />
           </Form.Fieldset>
+
           <Form.Fieldset className="form__lastname">
             <Form.Label labelFor="lastname" text="Último nome" />
             <Form.Input
               type="text"
-              required
               placeholder="Santos"
-              name="lastname"
-              id="lastname"
+              name="lastName"
+              id="lastName"
             />
+            <Form.ErrorMessage field="lastName" />
           </Form.Fieldset>
         </InputControlContainer>
+
         <Form.Fieldset className="form__email">
           <Form.Label labelFor="email" text="E-mail" />
           <Form.Input
             type="email"
-            required
             placeholder="vitordev@gmail.com"
             name="email"
             id="email"
           />
+          <Form.ErrorMessage field="email" />
         </Form.Fieldset>
+
         <InputControlContainer className="form__passwords-container flex">
           <Form.Fieldset className="form__password">
             <Form.Label labelFor="password" text="Senha" />
             <Form.Input
               type="password"
-              required
               name="password"
               id="password"
               placeholder="***************"
             />
+            <Form.ErrorMessage field="password" />
           </Form.Fieldset>
+
           <Form.Fieldset className="form__password--repeat">
             <Form.Label labelFor="password_repeat" text="Confirme sua Senha" />
             <Form.Input
               type="password"
-              required
-              name="password_repeat"
+              name="passwordRepeat"
               id="password_repeat"
               placeholder="***************"
             />
+            <Form.ErrorMessage field="passwordRepeat" />
           </Form.Fieldset>
         </InputControlContainer>
+
         <InputControlContainer className="form__content-creator-container flex">
           <p>Você é criador de conteúdo?</p>
           <Form.Fieldset className="form__content-creator flex">
             <Form.Label labelFor="yes" text="Sim" />
-            <Form.Input type="radio" required name="content-creator" id="yes" />
+            <Form.Input type="radio" name="content-creator" id="yes" />
             <Form.Label labelFor="no" text="Não" />
-            <Form.Input type="radio" required name="content-creator" id="no" />
+            <Form.Input type="radio" name="content-creator" id="no" />
           </Form.Fieldset>
         </InputControlContainer>
+
         <Form.Fieldset className="form__checkbox flex">
           <Form.Input
             type="checkbox"
---
+++ src/schemas/registerUserForm.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+export const registerUserFormSchema = z
+  .object({
+    firstName: z.string().nonempty('Insira seu primeiro nome!'),
+    lastName: z.string().nonempty('Insira seu último nome!'),
+    email: z
+      .string()
+      .nonempty('O e-mail é obrigatório!')
+      .email('Formato de e-mail inválido!'),
+    password: z
+      .string()
+      .nonempty('A senha é obrigatória')
+      .min(6, { message: 'A senha deve ter no mínimo 6 caracteres' }),
+    passwordRepeat: z.string().nonempty('A senha é obrigatória'),
+  })
+  .refine((data) => data.password === data.passwordRepeat, {
+    message: 'As senhas não coincidem!',
+    path: ['passwordRepeat'],
+  });
